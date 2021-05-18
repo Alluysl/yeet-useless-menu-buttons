17e0ee83b91f211f9cbe7138494a09e57725b994
Alluysl <alluysl.contact@gmail.com>
2021-05-18 23:20:28 +0200

    Mod now functional, and works with Mod Menu

--- src/main/java/net/fabricmc/example/ExampleMod.java
+++ src/main/java/net/fabricmc/example/ExampleMod.java
@@ -1,14 +1,50 @@
 package net.fabricmc.example;
 
-import net.fabricmc.api.ModInitializer;
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.screen.v1.Screens;
+import net.minecraft.client.gui.screen.GameMenuScreen;
+import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
+import net.minecraft.client.gui.widget.AbstractButtonWidget;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 
-public class ExampleMod implements ModInitializer {
+import java.util.List;
+
+public class ExampleMod implements ClientModInitializer {
 	@Override
-	public void onInitialize() {
+	public void onInitializeClient() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		System.out.println("Hello Fabric world!");
+		ScreenEvents.AFTER_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
+			if (screen instanceof GameMenuScreen) {
+				// If you want to create a button that's how you do it (with a few imports) for the record, I'll keep it there as a comment because WHY NOT
+//				Screens.getButtons(screen).add(new ButtonWidget(0, 0, 256, 20, new LiteralText("test"), new ButtonWidget.PressAction() {
+//					@Override
+//					public void onPress(ButtonWidget button) {
+//						// don't do anything
+//					}
+//				}));
+
+				// This is the brute method, that requires the indices to be changed everytime the menu changes or a mod adds buttons above (straight up removing the buttons makes the Mod Menu mod very sad by the way)
+//				Screens.getButtons(screen).get(3).visible = false;
+//				Screens.getButtons(screen).get(4).visible = false;
+
+				// The cleaner method that TerraformersMC's Mod Menu uses, credit is due etc etc
+				List<AbstractButtonWidget> buttons = Screens.getButtons(screen);
+				for (int i = 0; i < buttons.size(); i++) {
+					AbstractButtonWidget button = buttons.get(i);
+					if (buttonHasText(button, "menu.sendFeedback") || buttonHasText(button, "menu.reportBugs"))
+						button.visible = false;
+				}
+			}
+		});
+	}
+
+	// Shamelessly stolen from TerraformersMC's Mod Menu :>
+	private static boolean buttonHasText(AbstractButtonWidget button, String translationKey) {
+		Text text = button.getMessage();
+		return text instanceof TranslatableText && ((TranslatableText) text).getKey().equals(translationKey);
 	}
 }
